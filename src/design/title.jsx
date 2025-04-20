6854111f7e8a43b6cdc9c52748f02c3dee1a71ee
Mohamed Yusri <mohamedyusri205@gmail.com>
2025-04-21 01:30:58 +0200

    solving the title issue

--- src/design/title.jsx
+++ src/design/title.jsx
@@ -1,6 +1,7 @@
 const Title = ({ text ,className}) => {
     return (
-        <h2 className={`text-lg my-2 ${className}`}>{text}</h2>
+        // trying to solve the name of the file 
+        <h2 className={`text-lg my-2.5 ${className}`}>{text}</h2>
     );
 };
 
